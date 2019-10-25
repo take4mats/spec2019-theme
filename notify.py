8603c2346c34ba77af7ad2e9e6a4e7544c40549f
George <georgeorge.m@gmail.com>
2019-10-25 23:46:29 +0900

    fixed SQS event handling.  I hate myself with unnecessary haste, and it made waste!

--- notify.py
+++ notify.py
@@ -3,28 +3,17 @@ import os
 import uuid
 from datetime import datetime
 
-import boto3
 import requests
 
+
 def main(event, context):
-    print('hello are you alive?')
-    sqs = boto3.client('sqs')
-    q_url = os.environ['QUEUE_URL']
+    print('event: ')
+    print(json.dumps(event))
 
-    response = sqs.receive_message(
-        QueueUrl=q_url,
-        AttributeNames=[
-            'SentTimestamp'
-        ],
-        MaxNumberOfMessages=1,
-        VisibilityTimeout=0,
-        WaitTimeSeconds=0
-    )
+    for record in event['Records']:
+        print('-----')
+        data = json.loads(record['body'])
+        result = requests.post(os.environ['NOTIFICATION_ENDPOINT'], json=data)
+        print(result)
 
-    message = response['Messages'][0]
-    data = json.loads(message['Body'])
-    print(data)
-
-    result = requests.post(os.environ['NOTIFICATION_ENDPOINT'], json=data)
-    print(result)
     return
